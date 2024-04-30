083024da5011f44de5fb6d669795c499780bc41c
Brian Moscosa <bmoscosa@cps.edu>
2024-04-30 08:26:11 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Moscosa, Brian on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
